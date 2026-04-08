90654b70754dcc03e1a2b4d126aeff48ef2db60b
Jenerth Rodriguez <Connerjamestill@gmail.com>
2026-04-07 23:06:39 -0400

    Adding full project

--- src/main/java/com/Jenerth_market/HelloworldController.java
+++ src/main/java/com/Jenerth_market/HelloworldController.java
@@ -1,2 +1,15 @@
-package com.Jenerth_market;public class HelloworldController {
+package com.Jenerth_market;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greetings")
+
+public class HelloworldController {
+    @GetMapping("hello")
+    public String Hello(){
+     return "Hello World";
+    }
 }
---
+++ src/main/java/com/Jenerth_market/JenerthMarketApplication.java
@@ -0,0 +1,13 @@
+package com.Jenerth_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenerthMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenerthMarketApplication.class, args);
+	}
+
+}
