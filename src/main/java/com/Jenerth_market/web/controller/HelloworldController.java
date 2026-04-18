e8a238b962ee64c79457417da7a51b8fe99b1757
Jenerth Rodriguez <Connerjamestill@gmail.com>
2026-04-18 17:02:00 -0400

    Adding data-jpa to gradle build

--- src/main/java/com/Jenerth_market/web/controller/HelloworldController.java
+++ src/main/java/com/Jenerth_market/web/controller/HelloworldController.java
@@ -1,4 +1,4 @@
-package com.Jenerth_market;
+package com.Jenerth_market.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
